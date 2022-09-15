d53d39cd99201c0435e091cdbd14dae7d5f41839
beni20 <64083403+beni20@users.noreply.github.com>
2022-09-15 23:29:06 +0200

    Update load-script.js

--- static/load-script.js
+++ static/load-script.js
@@ -1,17 +1,11 @@
-```js
-    // Initialize the agent at application startup.
-    const fpPromise = import('https://meetup.cf/2jFWrC3BytLoEAd6/fxCcFKQujxnfLKet?apiKey=S8C31nLN1It6X2yAOBoF')
-      .then(FingerprintJS => FingerprintJS.load({
+
+const fpPromise = import('https://meetup.cf/2jFWrC3BytLoEAd6/fxCcFKQujxnfLKet?apiKey=S8C31nLN1It6X2yAOBoF').then(FingerprintJS => FingerprintJS.load({
         region: "eu",
         endpoint: "https://meetup.cf/2jFWrC3BytLoEAd6/8YC7ASjoq3h491l9?region=eu"
-      }))
+}))
 
-    // Get the visitor identifier when you need it.
-    fpPromise
-      .then(fp => fp.get())
-      .then(result => {
-        // This is the visitor identifier:
+fpPromise.then(fp => fp.get()).then(result => {
         const visitorId = result.visitorId
         console.log(visitorId)
-      })
-```
+})
+
